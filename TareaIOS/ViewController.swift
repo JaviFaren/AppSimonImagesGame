ef5c8e45406a451d2af450b2df05d96697020b92
Javier MAC <javier_pozuelo_m-tsapp1ma2324@cev.com>
2024-10-22 12:07:07 +0100

    Imágenes aleatorias mostradas, menu inicial creado y pantalla de marcadores agregada

--- TareaIOS/ViewController.swift
+++ TareaIOS/ViewController.swift
@@ -12,39 +12,48 @@ class ViewController: UIViewController {
     @IBOutlet weak var textImage: UILabel!
     @IBOutlet weak var Imageview: UIImageView!
     
-    @objc func timerFired() {
-          print("Timer Called with interval 30seconds")
-    }
+    let imagenesOrigen = [UIImage(named: "demonio"),
+                    UIImage(named: "duolingo"),
+                    UIImage(named: "franco"),
+                    UIImage(named: "banderatonga"),
+                    UIImage(named: "malphite"),
+                    UIImage(named: "mondongo"),
+                    UIImage(named: "neko"),
+                    UIImage(named: "vaporeon")]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.textImage.alpha = 0
         mostrarImagenes()
     }
 
     func mostrarImagenes(){
-        let imagenes = [UIImage(named: "demonio"),
-                        UIImage(named: "duolingo"),
-                        UIImage(named: "franco"),
-                        UIImage(named: "banderatonga"),
-                        UIImage(named: "malphite"),
-                        UIImage(named: "mondongo"),
-                        UIImage(named: "neko"),
-                        UIImage(named: "vaporeon")]
+        var imagenes = Array(imagenesOrigen)
+        
+        var imgRandom = [UIImage]()
+        for _ in imagenesOrigen{
+            let randNum = Int.random(in: 0..<(imagenes.count))
+            imgRandom.append(imagenes[randNum]!)
+            imagenes.remove(at: randNum)
+        }
+        
         var imgpos = 0
-        let temporizador = Timer.init(timeInterval: 3, repeats: true) { [self] (timer) in
-            print(imgpos)
-            Imageview.image = imagenes[imgpos]
-            textImage.text = "Imagen " + String(imgpos)
-            if imgpos == imagenes.count-1 {
-                timer.invalidate()
-                print("invalidado")
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (timer) in
+            if imgpos <= imgRandom.count-1{
+                self.Imageview.image = imgRandom[imgpos]
+                self.textImage.alpha = 1
+                self.textImage.text = "Imagen " + String(imgpos+1)
             }
-            imgpos = imgpos+1
+            if imgpos == imgRandom.count {
+                timer.invalidate()
+            }
+            else{
+                imgpos = imgpos+1
+            }
             
         }
         
-        temporizador.fire()
     }
 }
 
