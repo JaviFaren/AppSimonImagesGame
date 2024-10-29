5759f50f73c17b61aae373ff4622a4bdebd93d6e
Javier MAC <javier_pozuelo_m-tsapp1ma2324@cev.com>
2024-10-29 12:56:47 +0000

    avance soluciones user

--- TareaIOS/ViewController.swift
+++ TareaIOS/ViewController.swift
@@ -16,34 +16,6 @@ class ViewController: UIViewController {
     
     //Botones para elegir imagenes en orden
     var numeroSeleccion = -1
-    @IBAction func botonBandera(_ sender: Any) {
-        
-    }
-    @IBAction func botonDemonio(_ sender: Any) {
-        
-    }
-    @IBAction func botonFranco(_ sender: Any) {
-        
-    }
-    @IBAction func botonNeko(_ sender: Any) {
-        
-    }
-    
-    @IBAction func botonDuolingo(_ sender: Any) {
-        
-    }
-    @IBAction func botonMalphite(_ sender: Any) {
-        
-    }
-    @IBAction func botonMondongo(_ sender: Any) {
-        
-    }
-    @IBAction func botonVaporeon(_ sender: Any) {
-        
-    }
-    @IBAction func botonConfirmacion(_ sender: Any) {
-        
-    }
     
     let imagenesOrigen = [UIImage(named: "demonio"),
                     UIImage(named: "duolingo"),
@@ -54,11 +26,118 @@ class ViewController: UIViewController {
                     UIImage(named: "neko"),
                     UIImage(named: "vaporeon")]
     
+    var imagenSeleccionado = [Bool]()
+    var lastImage = [Int]()
+    
+    var respuestasUser = [Int]()
+    
+
+    @IBAction func DemonioButton(_ sender: Any) {
+        if !lastImage.isEmpty && lastImage[lastImage.count-1] == 0{
+            respuestasUser.remove(at: respuestasUser.count-1)
+            imagenSeleccionado[0] = false
+        }
+        else if !imagenSeleccionado[0]{
+            respuestasUser.append(0)
+            imagenSeleccionado[0] = true
+            lastImage[0] = 0
+        }
+        print("demonio")
+    }
+    
+    @IBAction func botonDuolingo(_ sender: Any) {
+        if !lastImage.isEmpty && lastImage[lastImage.count-1] == 1{
+            respuestasUser.remove(at: respuestasUser.count-1)
+            imagenSeleccionado[1] = false
+        }
+        else if !imagenSeleccionado[1]{
+            respuestasUser.append(1)
+            imagenSeleccionado[1] = true
+            lastImage[1] = 1
+        }
+    }
+    @IBAction func botonFranco(_ sender: Any) {
+        if !lastImage.isEmpty && lastImage[lastImage.count-1] == 2{
+            respuestasUser.remove(at: respuestasUser.count-1)
+            imagenSeleccionado[2] = false
+        }
+        else if !imagenSeleccionado[2]{
+            respuestasUser.append(2)
+            imagenSeleccionado[2] = true
+            lastImage[2] = 2
+        }
+    }
+    @IBAction func botonBandera(_ sender: Any) {
+        if !lastImage.isEmpty && lastImage[lastImage.count-1] == 3{
+            respuestasUser.remove(at: respuestasUser.count-1)
+            imagenSeleccionado[3] = false
+        }
+        else if !imagenSeleccionado[3]{
+            respuestasUser.append(3)
+            imagenSeleccionado[3] = true
+            lastImage[3] = 3
+        }
+    }
+    @IBAction func botonMalphite(_ sender: Any) {
+        if !lastImage.isEmpty && lastImage[lastImage.count-1] == 4{
+            respuestasUser.remove(at: respuestasUser.count-1)
+            imagenSeleccionado[4] = false
+        }
+        else if !imagenSeleccionado[4]{
+            respuestasUser.append(4)
+            imagenSeleccionado[4] = true
+            lastImage[4] = 4
+        }
+    }
+    @IBAction func botonMondongo(_ sender: Any) {
+        if !lastImage.isEmpty && lastImage[lastImage.count-1] == 5{
+            respuestasUser.remove(at: respuestasUser.count-1)
+            imagenSeleccionado[5] = false
+        }
+        else if !imagenSeleccionado[5]{
+            respuestasUser.append(5)
+            imagenSeleccionado[5] = true
+            lastImage[5] = 5
+        }
+    }
+    @IBAction func botonNeko(_ sender: Any) {
+        if !lastImage.isEmpty && lastImage[lastImage.count-1] == 6{
+            respuestasUser.remove(at: respuestasUser.count-1)
+            imagenSeleccionado[6] = false
+        }
+        else if !imagenSeleccionado[6]{
+            respuestasUser.append(6)
+            imagenSeleccionado[6] = true
+            lastImage[6] = 6
+        }
+    }
+    @IBAction func botonVaporeon(_ sender: Any) {
+        if !lastImage.isEmpty && lastImage[lastImage.count-1] == 7{
+            respuestasUser.remove(at: respuestasUser.count-1)
+            imagenSeleccionado[7] = false
+        }
+        else if !imagenSeleccionado[7]{
+            respuestasUser.append(7)
+            imagenSeleccionado[7] = true
+            lastImage[7] = 7
+        }
+    }
+
+    
+    @IBAction func botonConfirmacion(_ sender: Any) {
+        print(imagenSeleccionado)
+        print(respuestasUser)
+    }
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.textImage.alpha = 0
         self.botonContinuar.alpha = 0
+        for _ in imagenesOrigen{
+            imagenSeleccionado.append(false)
+        }
         mostrarImagenes()
     }
 
@@ -70,17 +149,15 @@ class ViewController: UIViewController {
         
         imagenesPos.shuffle()
         
-        let imagenesRandom = Array(imagenesPos)
-        
         
         var imgpos = 0
         Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { (timer) in
-            if imgpos <= imagenesRandom.count-1{
-                self.Imageview.image = self.imagenesOrigen[imagenesRandom[imgpos]]
+            if imgpos <= imagenesPos.count-1{
+                self.Imageview.image = self.imagenesOrigen[imagenesPos[imgpos]]
                 self.textImage.alpha = 1
                 self.textImage.text = "Imagen " + String(imgpos+1)
             }
-            if imgpos == imagenesRandom.count {
+            if imgpos == imagenesPos.count {
                 timer.invalidate()
                 self.botonContinuar.alpha = 1
             }
